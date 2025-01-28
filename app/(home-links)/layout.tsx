8e0a70735f30672aed98005662e95e2c7535e266
Halleluyah <halleluyaholudele@gmail.com>
2025-01-28 18:28:22 +0100

    removed unused imports

--- app/(home-links)/layout.tsx
+++ app/(home-links)/layout.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Noto_Sans } from "next/font/google";
 import "@/styles/globals.css";
-import ClientLayout from "./ClientLayout"; // We'll create this
-import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
